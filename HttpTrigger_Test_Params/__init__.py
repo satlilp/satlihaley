70573d2edf1955c08d7e3496ca746d85f1779580
satlilp <satli.ca@gmail.com>
2023-07-07 11:56:47 -0400

    HttpTrigger_Test_Params updated

--- HttpTrigger_Test_Params/__init__.py
+++ HttpTrigger_Test_Params/__init__.py
@@ -2,11 +2,10 @@ import logging
 
 import azure.functions as func
 
-
 # this script proves the req.params is used to receive values from the url
 # 1. click the url to jump to the webpage
-# 2. give the name and gender at the end of url, and enter
-# 3. then name and gender are retrieved from req.params
+# 2. give the name and gender at the end of url, and press enter
+# 3. then, name and gender are retrieved from req.params
 
 # next we need to test get and post method in the portal
 
@@ -37,10 +36,12 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
             print("5")
 
     if name:
+        # lst will give all the functions available under req.params
         lst = []
         for c in dir(req.params):
             if not c.startswith("_"):
                 lst.append(c)
+        # use req.params.get directly here inside the curly bracket
         return func.HttpResponse(f"Hello, {req.params.get('name')}. req.params is {req.params} and type_params is {type_params} and gender is {gender} and lst is {lst}")
     else:
         return func.HttpResponse(
